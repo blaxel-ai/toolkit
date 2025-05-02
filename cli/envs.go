1b6183c2078bf02874f7cfd32bcdaf705d825ce9
drappier-charles <cdrappier@beamlit.com>
2025-05-02 14:50:42 -0700

    Add ignore env variables on deploy

--- cli/envs.go
+++ cli/envs.go
@@ -1,20 +1,32 @@
 package cli
 
+import "slices"
+
 type Envs map[string]string
 type Env struct {
 	Name  string
 	Value string
 }
 
+var ignoredEnvs = []string{
+	"BL_API_KEY",
+}
+
 func GetEnvs() []Env {
 	var envs []Env
 	for _, secret := range secrets {
+		if slices.Contains(ignoredEnvs, secret.Name) {
+			continue
+		}
 		envs = append(envs, Env{
 			Name:  secret.Name,
 			Value: secret.Value,
 		})
 	}
 	for k, v := range config.Env {
+		if slices.Contains(ignoredEnvs, k) {
+			continue
+		}
 		envs = append(envs, Env{
 			Name:  k,
 			Value: v,
