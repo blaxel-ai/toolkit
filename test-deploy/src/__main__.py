1fc68f019de937d828ebc66c56671b5c812f6d30
cploujoux <cploujoux@blaxel.ai>
2025-06-06 12:49:59 -0700

    Remove test-deploy

--- test-deploy/src/__main__.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from blaxel import env
-
-port = env["BL_SERVER_PORT"]
-host = env["BL_SERVER_HOST"]
-
-if __name__ == "__main__":
-    uvicorn.run("src.main:app", host=host, port=int(port), reload=False)
