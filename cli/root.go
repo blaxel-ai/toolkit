528061f4f9d687705d2bec442060a075b76b75d6
cploujoux <cploujoux@blaxel.ai>
2025-06-13 12:50:22 -0700

    Fix run for sandboxes

--- cli/root.go
+++ cli/root.go
@@ -2,8 +2,10 @@ package cli
 
 import (
 	"github.com/blaxel-ai/toolkit/cli/core"
+	"github.com/joho/godotenv"
 )
 
 func Execute(releaseVersion string, releaseCommit string, releaseDate string) error {
+	godotenv.Load()
 	return core.Execute(releaseVersion, releaseCommit, releaseDate)
 }
